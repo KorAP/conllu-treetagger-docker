33721d0f6e0e20e4fd3ae2421c97ee2ca1af1405
Marc Kupietz <kupietz@ids-mannheim.de>
2025-11-22 09:16:14 +0100

    Remove upos column
    was just duplicated from xpos.
    
    Resolves #1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -51,5 +51,5 @@ if ! compgen -G  "/local/lib/${lang}*.par" > /dev/null; then
 fi
 
 perl -wlnpe'$_=substr($_, 0, 99000); s/^(#.*|$)/<$1>/; s/^[\d.]+\t([^\t]*).*/$1/' |  exec "tree-tagger-$lang" | \
- perl -wlne 's/^<(.*)>$/$1/; s/^(# *foundry *= *)base/$1 tree_tagger/; $id++; $id=0 if(/^(#|\s*$)/); my @cols = split("\t"); if(@cols > 2) { print "$id\t$cols[0]\t$cols[2]\t$cols[1]\t$cols[1]\t_\t_\t_\t_\t_"} else {print $_;}'
+ perl -wlne 's/^<(.*)>$/$1/; s/^(# *foundry *= *)base/$1 tree_tagger/; $id++; $id=0 if(/^(#|\s*$)/); my @cols = split("\t"); if(@cols > 2) { print "$id\t$cols[0]\t$cols[2]\t_\t$cols[1]\t_\t_\t_\t_\t_"} else {print $_;}'
 
